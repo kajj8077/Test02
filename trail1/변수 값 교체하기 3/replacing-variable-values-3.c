95a8ce3c63a45c660ecd711b11f952eb93085c63
kajj8077 <118797443+kajj8077@users.noreply.github.com>
2026-05-16 22:50:37 +0900

    [Passed] 변수 값 교체하기 3 (8ms, 0MB)

---
+++ trail1/변수 값 교체하기 3/replacing-variable-values-3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a = 3;
+    int b = 5;
+    int temp;
+    temp=a;
+    a=b;
+    b=temp;
+    printf("%d\n%d",a,b);
+    return 0;
+}
