b1587fa7b48fb744caeec5cbb51ea1f249a46431
kajj8077 <118797443+kajj8077@users.noreply.github.com>
2026-05-20 23:22:24 +0900

    [Passed] 문자열 순서 바꾸기 (2ms, 0MB)

---
+++ trail1/문자열 순서 바꾸기/change-order-of-strings.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    char S[100];
+    char T[100];
+    scanf("%s",S);
+    scanf("%s",T);
+    printf("%s\n%s",T,S);
+    return 0;
+}
